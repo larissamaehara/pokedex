e802e2e56ef57be10f8f87b13a4b20238999c013
Larissa Maehara <larissalali.m@gmail.com>
2023-06-07 17:59:16 -0300

    atualizando projeto

--- js/script.js
+++ js/script.js
@@ -6,25 +6,25 @@ const form = document.querySelector('.form');
 const input = document.querySelector('.input__search');
 const buttonPrev = document.querySelector('.btn-prev');
 const buttonNext = document.querySelector('.btn-next');
-
+//Inicia com o Pokemon 1
 let searchPokemon = 1;
-
+//Função que faz a busca do Pokemon na API
 const fetchPokemon = async (pokemon) => {
   const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
 
   if (APIResponse.status === 200) {
-    const data = await APIResponse.json();
+    const data = await APIResponse.json(); //extrai dados da API
     return data;
   }
 }
-
+//Função que renderiza na tela os dados
 const renderPokemon = async (pokemon) => {
 
   pokemonName.innerHTML = 'Loading...';
-  pokemonNumber.innerHTML = '';
+  pokemonNumber.innerHTML = ''; //iniciar número vazio
 
-  const data = await fetchPokemon(pokemon);
-
+  const data = await fetchPokemon(pokemon); //recupera os dados
+// condição para caso não encontre o pokemon procurado, retorne uma mensagem
   if (data) {
     pokemonImage.style.display = 'block';
     pokemonName.innerHTML = data.name;
@@ -33,12 +33,12 @@ const renderPokemon = async (pokemon) => {
     input.value = '';
     searchPokemon = data.id;
   } else {
-    pokemonImage.style.display = 'none';
-    pokemonName.innerHTML = 'Not found :c';
+    pokemonImage.style.display = 'none'; // Esconde imagem
+    pokemonName.innerHTML = 'Sorry, not found =(';
     pokemonNumber.innerHTML = '';
   }
 }
-
+//Adicionando eventos, formulário, botões...
 form.addEventListener('submit', (event) => {
   event.preventDefault();
   renderPokemon(input.value.toLowerCase());
